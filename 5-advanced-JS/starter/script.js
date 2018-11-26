6c6baa0620b976ef74c1d798cccbcaa507cf8363
Michael Salvati <print@3daddict.com>
2018-11-26 09:30:00 -0800

    primitives vs objects

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -26,19 +26,55 @@
 // console.log(mark.lastName);
 
 //Object.create
-var personProto = {
-    calculateAge: function(){
-        console.log(2016 - this.yearOfBirth);
-    }
+// var personProto = {
+//     calculateAge: function(){
+//         console.log(2016 - this.yearOfBirth);
+//     }
+// };
+
+// var john = Object.create(personProto);
+// john.name = 'John';
+// john.yearOfBirth = 1990;
+// john.job = "Teacher";
+
+// var jane = Object.create(personProto, {
+//     name: { value: 'Jane'},
+//     yearOfBirth: { value: 1969},
+//     job: { value: 'Designer'}
+// });
+
+//Primitives vs Objects
+
+//Primitives
+var a = 23;
+var b = a;
+a = 46;
+console.log(a);
+console.log(b);
+
+//Objects
+var obj1 = {
+    name: 'John',
+    age: 26
 };
 
-var john = Object.create(personProto);
-john.name = 'John';
-john.yearOfBirth = 1990;
-john.job = "Teacher";
+var obj2 = obj1;
+obj1.age = 30;
+console.log(obj1.age);
+console.log(obj2.age);
 
-var jane = Object.create(personProto, {
-    name: { value: 'Jane'},
-    yearOfBirth: { value: 1969},
-    job: { value: 'Designer'}
-});
+//Functions
+var age = 27;
+var obj = {
+    name: "Jonas",
+    city: 'Lisbon'
+}
+
+function change(a, b){
+    a = 30;
+    b.city = 'San Fancisco';
+}
+
+change(age, obj);
+console.log(age);
+console.log(obj.city);
