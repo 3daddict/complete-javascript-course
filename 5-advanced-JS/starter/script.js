2932c7ba52a20438dc18549e3dece54b33413c46
Michael Salvati <print@3daddict.com>
2018-11-26 09:45:36 -0800

    Passing functions as arguments created

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -43,38 +43,74 @@
 //     job: { value: 'Designer'}
 // });
 
-//Primitives vs Objects
+// //Primitives vs Objects
 
-//Primitives
-var a = 23;
-var b = a;
-a = 46;
-console.log(a);
-console.log(b);
+// //Primitives
+// var a = 23;
+// var b = a;
+// a = 46;
+// console.log(a);
+// console.log(b);
 
-//Objects
-var obj1 = {
-    name: 'John',
-    age: 26
-};
+// //Objects
+// var obj1 = {
+//     name: 'John',
+//     age: 26
+// };
 
-var obj2 = obj1;
-obj1.age = 30;
-console.log(obj1.age);
-console.log(obj2.age);
+// var obj2 = obj1;
+// obj1.age = 30;
+// console.log(obj1.age);
+// console.log(obj2.age);
 
-//Functions
-var age = 27;
-var obj = {
-    name: "Jonas",
-    city: 'Lisbon'
+// //Functions
+// var age = 27;
+// var obj = {
+//     name: "Jonas",
+//     city: 'Lisbon'
+// }
+
+// function change(a, b){
+//     a = 30;
+//     b.city = 'San Fancisco';
+// }
+
+// change(age, obj);
+// console.log(age);
+// console.log(obj.city);
+
+//Passing functions as arguments
+var years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn){
+    var arrRes = [];
+    for(var i in years){
+        arrRes.push(fn(arr[i]));
+    }
+    return arrRes;
 }
 
-function change(a, b){
-    a = 30;
-    b.city = 'San Fancisco';
+function calculateAge(el){
+    return 2016 - el;
 }
 
-change(age, obj);
-console.log(age);
-console.log(obj.city);
+function isFullAge(el){
+    return el >= 18;
+}
+
+function maxHeartRate(el){
+    if (el >= 18 && el <= 81) {
+        return Math.round(206.9 - (0.67 * el));
+    } else {
+        return -1;
+    }
+}
+
+var ages = arrayCalc(years, calculateAge);
+console.log(ages);
+
+var fullAges = arrayCalc(ages, isFullAge);
+console.log(fullAges);
+
+var rates = arrayCalc(ages, maxHeartRate);
+console.log(rates);
