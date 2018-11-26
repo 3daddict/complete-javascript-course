808573834fa62f7b1c265c0699c082ec9a13c453
Michael Salvati <print@3daddict.com>
2018-11-26 14:10:15 -0800

    closures example created

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -164,7 +164,26 @@ function retirement(retirementAge) {
     }
 }
 
-// var retirementUS = retirement(66);
-// retirementUS(1990);
+var retirementUS = retirement(66);
+var retirementDE = retirement(65);
+var retirementIceland = retirement(67);
 
-retirement(66)(1990);
+retirementUS(1990);
+retirementDE(1990);
+retirementIceland(1990);
+
+function interviewQuestion(job){
+    return function(name){
+        if(job === 'designer') {
+            console.log(name, ', Can you please explain what UX design is?')
+        } else if(job === 'teacher') {
+            console.log('what subject do you teach', name);
+        } else {
+            console.log('Hello', name, 'what do you do?');
+        }
+    }
+}
+
+interviewQuestion('designer')('John');
+interviewQuestion('teacher')('Jane');
+interviewQuestion('drug dealer')('Mark');
